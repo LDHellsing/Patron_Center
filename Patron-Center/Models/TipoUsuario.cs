3a00259ae2080fda27660183b042f4b5a22ab97d
Gonzalo Sosa <gonchi94@gmail.com>
2019-06-27 12:19:49 -0300

    test de git

--- Patron-Center/Models/TipoUsuario.cs
+++ Patron-Center/Models/TipoUsuario.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 
 namespace Patron_Center.Models
 {
+    /// <summary>
+    /// Borrar esto es para probar el git
+    /// </summary>
     public enum TipoUsuario
     {
         Alumno,
