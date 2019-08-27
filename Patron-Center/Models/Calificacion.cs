a95b75255ca150966d28a9e83d91800a52faa56a
Luis Machado <infinitesubstance@hotmail.com>
2019-08-27 19:11:27 -0300

    Conflictos

---
+++ Patron-Center/Models/Calificacion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Patron_Center.Models
+{
+    public class Calificacion
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public int UnidadId { get; set; }
+        public int CursoId { get; set; }
+        public DateTime Fecha { get; set; }
+        //Calificacion final de la evaluacion
+        public int Nota { get; set; }
+    }
+}
---
+++ Patron-Center/Models/RespuestaAlumno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Patron_Center.Models
+{
+    public class RespuestaAlumno
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public int PreguntaId { get; set; }
+        public int RespuestaId { get; set; }
+        public string RespuestaDesarrollo { get; set; }
+    }
+}
