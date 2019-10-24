3193094430a78745790f8b376731c68f42cc6149
Luis Machado <infinitesubstance@hotmail.com>
2019-10-24 19:53:21 -0300

    Estitica calificaciones

--- Patron-Center/Controllers/CalificacionesController.cs
+++ Patron-Center/Controllers/CalificacionesController.cs
@@ -23,6 +23,18 @@ namespace Patron_Center.Controllers
         // GET: Calificaciones
         public async Task<IActionResult> Index()
         {
+            if (HttpContext.Session.GetInt32("_IdUsuario") == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            else
+            {
+                ViewBag.Nombre = HttpContext.Session.GetString("_Nombre");
+                ViewBag.IdUsuario = HttpContext.Session.GetInt32("_IdUsuario");
+                ViewBag.TipoUsuario = HttpContext.Session.GetString("_TipoUsuario");
+            }
+
+
             int idUsuario = (int) HttpContext.Session.GetInt32("_IdUsuario");
             var tipoUsuario = HttpContext.Session.GetString("_TipoUsuario");
 
@@ -30,127 +42,6 @@ namespace Patron_Center.Controllers
 
             return View(calificaciones);
         }
-
-        // GET: Calificaciones/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var calificacion = await _context.Calificacion
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (calificacion == null)
-            {
-                return NotFound();
-            }
-
-            return View(calificacion);
-        }
-
-        // GET: Calificaciones/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Calificaciones/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,IdAlumno,IdUnidad,IdCurso,Fecha,Nota")] Calificacion calificacion)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(calificacion);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(calificacion);
-        }
-
-        // GET: Calificaciones/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var calificacion = await _context.Calificacion.FindAsync(id);
-            if (calificacion == null)
-            {
-                return NotFound();
-            }
-            return View(calificacion);
-        }
-
-        // POST: Calificaciones/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,IdAlumno,IdUnidad,IdCurso,Fecha,Nota")] Calificacion calificacion)
-        {
-            if (id != calificacion.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(calificacion);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CalificacionExists(calificacion.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(calificacion);
-        }
-
-        // GET: Calificaciones/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var calificacion = await _context.Calificacion
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (calificacion == null)
-            {
-                return NotFound();
-            }
-
-            return View(calificacion);
-        }
-
-        // POST: Calificaciones/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var calificacion = await _context.Calificacion.FindAsync(id);
-            _context.Calificacion.Remove(calificacion);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
         private bool CalificacionExists(int id)
         {
             return _context.Calificacion.Any(e => e.Id == id);
